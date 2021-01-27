1904c19b3b6d1f2720f216529fa5cb71bf8fb421
Kevaughn Clarke <k.clarke0894@gmail.com>
2021-01-26 23:26:07 -0500

    added animation for favorite icon when clicked on

--- src/Components/Favorite/Favorite.js
+++ src/Components/Favorite/Favorite.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
+import './Favorite.css';
+
 export default function Favorite(props) {
   const [favorite, setFavorite] = useState(props.isFav);
 
